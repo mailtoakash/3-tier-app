6b732a5891a8a888ad41c3cac70e8b8d0ec01177
Akash kannan <162781145+mailtoakash@users.noreply.github.com>
2026-06-01 16:27:31 +0530

    Fix URL syntax in deleteBook function

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -14,7 +14,7 @@ const DeleteBook = () => {
   const handleDeleteBook = () => {
     setLoading(true);
     axios
-      .delete(`http://52.66.213.32:5555/books'/${id}`)
+      .delete(`http://52.66.213.32:5555/books/${id}`)
       .then(() => {
         setLoading(false);
         enqueueSnackbar('Book Deleted successfully', { variant: 'success' });
