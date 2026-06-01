7dde5492f8f62820eeb3f60103da7696797b7b31
Akash kannan <162781145+mailtoakash@users.noreply.github.com>
2026-06-01 16:27:53 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get('http://localhost:5555/books')
+      .get('http://52.66.213.32:5555/books')
       .then((response) => {
         setBooks(response.data.data);
         setLoading(false);
