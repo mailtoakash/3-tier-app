96d2e794c7020633ad6fbf89933c78e6d4f0b776
Akash kannan <162781145+mailtoakash@users.noreply.github.com>
2026-06-01 16:28:17 +0530

    Update API endpoint for fetching book details

--- frontend/src/pages/ShowBook.jsx
+++ frontend/src/pages/ShowBook.jsx
@@ -12,7 +12,7 @@ const ShowBook = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get(`http://localhost:5555/books/${id}`)
+      .get(`http://52.66.213.32:5555/books/${id}`)
       .then((response) => {
         setBook(response.data);
         setLoading(false);
